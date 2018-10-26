5d277a31a9953f0c15a35cdbaccdfbe903c7b1f0
Ujjwal Gupta <ugupta41@gmail.com>
2018-10-26 23:25:35 +0530

    Added series, events link to marvel class

--- marvel/marvel.py
+++ marvel/marvel.py
@@ -1,8 +1,13 @@
 from marvel.base import Base
 from marvel.modules.characters import Characters
+from marvel.modules.events import Events
+from marvel.modules.series import Series
 
 
 class Marvel(Base):
     def __init__(self, PUBLIC_KEY, PRIVATE_KEY):
         super().__init__(PUBLIC_KEY, PRIVATE_KEY)
         self.characters = Characters(requester=self.requester)
+        self.events = Events(requester=self.requester)
+        self.series = Series(requester=self.requester)
+
