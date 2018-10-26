c193f464f3b75c29129b1d9864ed3754ed269d71
Ujjwal Gupta <ugupta41@gmail.com>
2018-10-26 23:24:06 +0530

    Added couple of more modules

---
+++ marvel/modules/series.py
@@ -0,0 +1,34 @@
+from marvel.modules.base_module import BaseModule
+
+
+class Series(BaseModule):
+    def __init__(self, requester):
+        super().__init__(requester)
+
+    def all(self):
+        data, headers = self.r.request('series')
+        return data
+
+    def get(self, identifier, **kwargs):
+        data, headers = self.r.request('series', identifier=identifier, payload=kwargs)
+        return data
+
+    def characters(self, identifier, **kwargs):
+        data, headers = self.r.request('series', identifier=identifier, payload=kwargs, sub_endpoint="characters")
+        return data
+
+    def comics(self, identifier, **kwargs):
+        data, headers = self.r.request('series', identifier=identifier, payload=kwargs, sub_endpoint="comics")
+        return data
+
+    def creators(self, identifier, **kwargs):
+        data, headers = self.r.request('series', identifier=identifier, payload=kwargs, sub_endpoint="creators")
+        return data
+
+    def events(self, identifier, **kwargs):
+        data, headers = self.r.request('series', identifier=identifier, payload=kwargs, sub_endpoint="events")
+        return data
+
+    def stories(self, identifier, **kwargs):
+        data, headers = self.r.request('series', identifier=identifier, payload=kwargs, sub_endpoint="stories")
+        return data
